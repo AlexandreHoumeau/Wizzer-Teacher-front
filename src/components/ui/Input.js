06deefafdb51c4948c0acafab07f1a7dd9b782ba
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-11-18 18:26:45 +0100

    [AC-19] created input component

---
+++ src/components/ui/Input.js
@@ -0,0 +1,31 @@
+import React from "react";
+import * as Icons from "assets/icons";
+
+const Input = ({ placeholder, error, value, label, icon }) => {
+  function Icon({ icon, ...props }) {
+    const Icon = Icons[icon];
+    return <Icon {...props} />;
+  }
+
+  return (
+    <>
+      <div className="border-b-2 border-grey-dark">
+        <p className=" font-raleway text-base text-grey-dark font-medium">{label}</p>
+        <div className="flex my-2 mb-2 items-center">
+          <Icon className="w-6 h-6 mr-2" aria-hidden="true" icon={icon} />
+          <input
+            className="focus:outline-none font-raleway font-medium placeholder-grey-darker"
+            placeholder={placeholder}
+          />
+        </div>
+      </div>
+      {error && (
+        <div>
+          <p className="text-error font-raleway mt-2">{error}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Input;
