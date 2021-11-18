1705cb35aac5edfb684c7931480c723b71b4295c
HariTharmalingam <lqjhari@gmail.com>
2021-11-18 12:15:59 +0100

    Added fonts and colors

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="font-raleway font-medium bg-primary text-primary-light">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
