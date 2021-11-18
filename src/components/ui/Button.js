a400af67c084a0154765615c6dd5e8b3a31f579e
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-11-18 18:43:49 +0100

    [AC-13] created button component

---
+++ src/components/ui/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({ text, type, action }) => {
+  return (
+    <button onClick={action} className={`bg-${type} py-4 px-8 rounded-4xl`}>
+      <p className="text-lg font-raleway font-bold text-waiting-light">{text}</p>
+    </button>
+  )
+}
+
+export default Button
