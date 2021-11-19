0a968bac17f99e329a64f347b60e006c7b7c3122
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-11-19 11:28:50 +0100

    [AC-13] added icon contact

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,9 +2,11 @@
 import { ReactComponent as MailIcon } from './mail.svg'
 import { ReactComponent as EyeIcon } from './eye.svg'
 import { ReactComponent as LockIcon } from './lock.svg'
+import { ReactComponent as ContactIcon } from './contact.svg'
 
 export {
   MailIcon,
   EyeIcon,
-  LockIcon
+  LockIcon,
+  ContactIcon
 }
