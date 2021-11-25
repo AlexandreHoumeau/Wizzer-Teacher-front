bb94a1235df1f8a2c0a92ac60b47ff5c1e7ce996
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-11-25 18:12:55 +0100

    Updated Private route

--- src/routes/privateRoutes.js
+++ src/routes/privateRoutes.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { Route, Redirect} from 'react-router-dom';
-import { isLogin } from '../utils/isLogin';
+import Unauthorized from "pages/Shared/Unauthorized";
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { isLogin } from "utils/isLogin";
+import hasRoles from "utils/hasRoles";
 
-const PrivateRoute = ({component: Component, roles, path, ...rest}) => {
-    roles = roles || [];
-    return (
-        <Route {...rest} render={props => (
-            isLogin() ?
-                <Component {...props} />
-            : <Redirect to="/login" />
-        )} />
-    );
+const PrivateRoute = ({ component: Component, roles, path, ...rest }) => {
+  roles = roles || [];
+  return (
+    <Route
+      {...rest}
+      render={(props) => 
+        hasRoles(roles) ? (
+          <Component {...props} />
+        ) : isLogin() ?  <Unauthorized /> : <Redirect to="/login" />
+      }
+    />
+  );
 };
 
 export default PrivateRoute;
@@ -22,7 +27,7 @@ export default PrivateRoute;
 //         <Route
 //             path={path}
 //             exact={true}
-//             render={(props) => 
+//             render={(props) =>
 //                 hasRoles(roles) ? (
 //                     <Component {...props} />
 //                 ) : (
