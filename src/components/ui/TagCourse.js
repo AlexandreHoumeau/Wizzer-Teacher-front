f8bcaedf1bdc56c9a6432e663744ed644b628282
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-11-29 23:55:01 +0100

    Created component ui

---
+++ src/components/ui/TagCourse.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TagCourse = ({ title }) => {
+  return (
+    <div className='bg-primary-light px-4 py-2 rounded-4xl'>
+      <p className='text-primary font-raleway font-medium'>{title}</p>
+    </div>  )
+}
+
+export default TagCourse
