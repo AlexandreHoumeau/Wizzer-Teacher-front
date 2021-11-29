f0a978c2f93a2e6b714e349c564d70abeb9c69d3
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-11-29 23:56:53 +0100

    Updated Dashboard layout

--- src/pages/Dashboard/Main/index.js
+++ src/pages/Dashboard/Main/index.js
@@ -1,14 +1,15 @@
 // import ThemedSuspense from "components/ui/ThemedSuspense";
 import React, { Suspense } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import { connect } from "react-redux";
+
 import Loader from "components/ui/Loader";
 import PrivateRoute from "routes/privateRoutes";
 
 const Main = ({ routes, isLoading }) => {
   return (
-    <main className="h-full bg-purple-50 overflow-y-auto">
-      <div className="relative grid">
+    <main className="h-full">
+      <div className="relative px-36 py-12">
         <Suspense fallback={<Loader isLoading={isLoading}/>} />
           <Switch>
             {routes.map((route, i) => {
