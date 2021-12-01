5f0d71a4ebba1c8234cb7680ca788da76942198f
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-12-01 23:31:12 +0100

    Updated ThemedSuspense

--- src/components/ui/ThemedSuspense.js
+++ src/components/ui/ThemedSuspense.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 function ThemedSuspense() {
   return (
-    <div className="w-full h-screen p-6 text-lg font-medium text-gray-600 dark:text-gray-400 dark:bg-gray-900">
-      Loading...
+    <div className="w-full h-full backdrop-filter flex justify-center items-center backdrop-blur-sm absolute z-50">
+      <div className="">
+        <Player
+          autoplay
+          loop
+          src="https://assets1.lottiefiles.com/temporary_files/Fpip5r.json"
+          className=" w-1/2 z-50"
+        ></Player>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ThemedSuspense
+export default ThemedSuspense;
