dd6402d5d02ae4f4bfef10dcf13ebec5d0286ca0
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-12-02 23:21:00 +0100

    Updated new module page

--- src/pages/Admin/Modules/NewModules/index.js
+++ src/pages/Admin/Modules/NewModules/index.js
@@ -7,71 +7,21 @@ import { useState } from "react";
 
 const NewModule = () => {
   const [checked, setChecked] = useState(false);
+  const [people, setPeople] = useState([]);
   const handleClick = () => {
     setChecked(!checked);
   };
 
   return (
-    <>
-      <div className="mt-10 sm:mt-0">
-        <div className="md:grid md:grid-cols-3 md:gap-6">
-          <div className="md:col-span-1">
-            <div className="px-4 sm:px-0">
-              <h3 className="text-lg font-medium font-raleway leading-6 text-gray-900">
-                Informations du module
-              </h3>
-              <p className="mt-1 font-raleway text-sm text-gray-600">
-                Merci de renseigner les informations basiques du module.
-              </p>
-            </div>
-          </div>
-          <div className="mt-5 md:mt-0 md:col-span-2">
-            <form action="#" method="POST">
-              <div className="shadow overflow-hidden sm:rounded-md">
-                <div className="px-4 py-5 bg-white sm:p-6">
-                  <div className="grid grid-cols-6 gap-6">
-                    <div className="col-span-6 sm:col-span-4">
-                      <Input label="Titre du module" placeholder="Taper ici" />
-                    </div>
-                    <div className="col-span-6 sm:col-span-4">
-                      <Input label="Description du module" placeholder="Taper ici" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+    <div className="flex flex-col text-left">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
 
-      <div className="hidden sm:block" aria-hidden="true">
-        <div className="py-5">
-          <div className="border-t border-gray-200" />
-        </div>
-      </div>
-
-      <div className="mt-10 sm:mt-0">
-        <div className="md:grid md:grid-cols-3 md:gap-6">
-          <div className="md:col-span-1">
-            <div className="px-4 sm:px-0">
-              <h3 className="text-lg font-medium leading-6 text-gray-900">
-                Les cours du module
-              </h3>
-              <p className="mt-1 text-sm text-gray-600">
-                Ici vous pouvez commencer à renseigner les cours et exercices
-              </p>
-            </div>
-          </div>
-          <div className="mt-5 md:mt-0 md:col-span-2">
-            <form action="#" method="POST">
-              <div className="shadow overflow-hidden sm:rounded-md">
-                <div>Hello World</div>
-              </div>
-            </form>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
