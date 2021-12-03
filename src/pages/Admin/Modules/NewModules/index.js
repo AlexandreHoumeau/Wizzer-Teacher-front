f85b1a10ba79d1f959578eb21ed9acd3a9a0aa43
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-12-03 18:34:11 +0100

    Updated new module

--- src/pages/Admin/Modules/NewModules/index.js
+++ src/pages/Admin/Modules/NewModules/index.js
@@ -1,26 +1,62 @@
 import React from "react";
-import { Input, Button } from "components/ui";
-
-import { Switch } from "antd";
-import { CloseOutlined, CheckOutlined } from "@ant-design/icons";
+import { Table, Button } from "components/ui"
 import { useState } from "react";
 
 const NewModule = () => {
-  const [checked, setChecked] = useState(false);
-  const [people, setPeople] = useState([]);
-  const handleClick = () => {
-    setChecked(!checked);
-  };
+
+  const dataSource = [
+    {
+      key: '1',
+      title: 'Mike',
+      points: 32,
+      status: '10 Downing Street',
+    },
+    // {
+    //   key: '2',
+    //   name: 'John',
+    //   age: 42,
+    //   address: '10 Downing Street',
+    // },
+  ];
+
+  const columns = [
+    {
+      title: "Titre du cours",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Points",
+      dataIndex: "points",
+      key: "points",
+    },
+    {
+      title: "Statut",
+      dataIndex: "status",
+      key: "status",
+    },
+    {
+      title: "Battle",
+      dataIndex: "battle",
+      key: "battle",
+    },
+  ];
 
   return (
-    <div className="flex flex-col text-left">
+    <div className="">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-
+          <div className="flex justify-between mb-10">
+            <div className="text-left font-raleway font-bold text-2xl mb-5">Ajouter un module</div>
+            <div className="border-2 border-grey-dark cursor-pointer py-3 px-4 rounded-md">
+              
+              <div className="text-gray-400 font-bold">Ajouter un cours</div>
+            </div>
           </div>
         </div>
       </div>
+
+      <Table dataSource={dataSource} columns={columns} />
     </div>
   );
 };
