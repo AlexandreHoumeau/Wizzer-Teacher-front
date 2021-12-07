f3c3441005d4ca30dde628d7397c4597a15dabfa
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-12-07 16:07:08 +0100

    Updated button component

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({ text, type, action }) => {
+const Button = ({ text, type, action, disabled }) => {
   return (
     <button type="submit" onClick={action} className={`bg-${type} py-4 px-8 rounded-4xl`}>
       <p className="text-lg font-raleway font-bold text-waiting-light">{text}</p>
