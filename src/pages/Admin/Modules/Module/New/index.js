16e669ad60a3fc0830943c5d64d83eb51f35074a
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-12-09 10:22:58 +0100

    Updated new course page

--- src/pages/Admin/Modules/Module/New/index.js
+++ src/pages/Admin/Modules/Module/New/index.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import { Input } from "components/ui";
+import React from "react";
 
 const NewCourse = () => {
   return (
     <div>
-      New course page
-    </div>
-  )
-}
+      <div className="mb-10 flex items-center justify-between">
+        <div className="font-raleway font-bold text-3xl">Créer un cours</div>
+      </div>
 
-export default NewCourse
+      <div>
+        <div>Partie cours</div>
+        <Input label="Titre du cours" placeholder="Titre" />
+        <Input label="Url du google doc" placeholder="url" />
+      </div>
+    </div>
+  );
+};
+
+export default NewCourse;
