e4ef78f2b575d1a3f6b761241338c64b7dbe7f25
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-02-02 22:48:40 +0100

    added disabled system

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import classNames from "classnames";
+import React from "react";
 
 const Button = ({ text, type, action, disabled }) => {
   return (
-    <button type="submit" onClick={action} className={`bg-${type} py-4 px-8 rounded-4xl`}>
-      <p className="text-lg font-raleway font-bold text-waiting-light">{text}</p>
+    <button
+      type="submit"
+      onClick={action}
+      className={classNames(
+        disabled ? `bg-${type}-disabled` : `bg-${type}`,
+        'py-4 px-8 rounded-4xl'
+        )}
+      // className={`bg-${type} py-4 px-8 rounded-4xl`}
+    >
+      <p className="text-lg font-raleway font-bold text-white">
+        {text}
+      </p>
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
