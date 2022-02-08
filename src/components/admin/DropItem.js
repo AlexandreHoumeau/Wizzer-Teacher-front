5df10cb0f52e5b8c0f0095a43c47cc8c5c02dd18
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-02-08 12:12:45 +0100

    Updated dropItem

--- src/components/admin/DropItem.js
+++ src/components/admin/DropItem.js
@@ -1,34 +1,68 @@
+import React, { useState, useEffect } from "react";
 import classNames from "classnames";
-import React from "react";
 import { useDrop } from "react-dnd";
+import { RecycleBinIcon } from "assets/icons";
 
-const DropItem = ({ index }) => {
+const DropItem = ({ index, addCourse, courses, removeCourse }) => {
+  const [hover, setHover] = useState(false);
   const [{ canDrop, isOver }, drop] = useDrop(() => ({
     // The type (or types) to accept - strings or symbols
     accept: "BOX",
     // Props to collect
-    drop: (item, monitor) => getItem(item),
+    drop: (item, monitor) => addCourse(item, index),
     collect: (monitor) => ({
       isOver: monitor.isOver(),
       canDrop: monitor.canDrop(),
     }),
   }));
 
-  const getItem = (item) => {
-    console.log(item)
-  }
+  useEffect(() => {
+    console.log(courses);
+  }, [courses]);
 
   return (
     <div
       ref={drop}
       role={"Dustbin"}
-      style={{ backgroundColor: isOver ? "red" : "" }}
       className={classNames(
+        isOver && "bg-purple-100",
         index === 0 ? "" : "border-l",
-        "text-center p-3 italic text-grey-dark"
+        "text-center italic h-full"
       )}
     >
-      Ajouter
+      {courses?.map((course, indexCourse) => (
+        <div
+          key={indexCourse}
+          onMouseEnter={() => setHover(indexCourse)}
+          onMouseLeave={() => setHover(null)}
+          onClick={() => removeCourse(course, index)}
+          className={classNames(
+            hover === indexCourse ? "bg-purple-50" : "bg-purple-100",
+            "transform cursor-pointer relative duration-200 text-purple-500 p-3 text-left font-thin not-italic rounded-lg m-2 font"
+          )}
+        >
+          {hover === indexCourse && (
+              <RecycleBinIcon className="absolute"/>
+          )}
+          <div className={classNames(hover === indexCourse && 'opacity-20')}>
+            <div>{course.title}</div>
+            <div className="flex items-center justify-between">
+              <div className="font-bold">{course._module.title}</div>
+              <div
+                className={classNames(
+                  course.difficulty === "hard"
+                    ? "bg-red"
+                    : course.difficulty === "medium"
+                    ? "bg-blue-500"
+                    : "bg-green-500",
+                  "w-2 h-2 rounded-full"
+                )}
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+      <div className="py-5 text-gray-400 p-3"/>
     </div>
   );
 };
