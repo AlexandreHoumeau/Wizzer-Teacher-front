f8f2bc4289fda7fed9839cbf0c8dc91c67c40a5d
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-02-12 14:12:01 +0100

    updated module page

--- src/pages/Modules/index.js
+++ src/pages/Modules/index.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import { Banner } from "components/modules";
+import Card from "components/modules/Card";
+import React, { useEffect, useState } from "react";
+import api from "services/api";
 
 const Modules = () => {
+  const [modules, setModules] = useState({});
+  const fetchModules = async () => {
+    const data = await api.axios.get("/v1/user/modules");
+    if (data) {
+      setModules(data);
+    }
+  };
+  useEffect(() => {
+    fetchModules();
+  }, []);
   return (
-    <div>
-      Hello World from modules page
-    </div>
-  )
-}
+    <div className="font-raleway">
+      <div>
+        <Banner />
+      </div>
+      {modules.dev && (
+        <div>
+          <div className="text-3xl font-semibold mb-8">
+            Modules developpement
+          </div>
+          <div className="flex">
+            {modules.dev.map(
+              (module) =>
+                module._exercices.length > 0 && (
+                  <Card key={module._id} module={module} />
+                )
+            )}
+          </div>
+        </div>
+      )}
 
-export default Modules
+      {modules.design && (
+        <div className="mt-10">
+          <div className="text-3xl font-semibold mb-8">
+            Modules design
+          </div>
+          <div className="flex">
+            {modules.design.map(
+              (module) =>
+                module._exercices.length > 0 && (
+                  <Card key={module._id} module={module} />
+                )
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Modules;
