ff0939000f651c5577a5d8613f98dfca68f49881
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-02-15 23:57:34 +0100

    updated card component

--- src/components/modules/Card.js
+++ src/components/modules/Card.js
@@ -3,9 +3,15 @@ import React from "react";
 import { useHistory } from "react-router";
 
 const Card = ({ module }) => {
-  const history = useHistory()
+  const history = useHistory();
+  const nmbTests = module._exercices
+    .map((exercice) => exercice._tests)
+    .filter((element) => element?.length).length;
   return (
-    <div onClick={() => history.push(`modules/${module._id}`)} className="bg-grey-light cursor-pointer hover:border-primary border-transparent transform duration-150 border flex-col flex justify-between relative w-72 h-56 mr-6 px-8 py-6 rounded-3xl font-raleway">
+    <div
+      onClick={() => history.push(`modules/${module._id}`)}
+      className="bg-grey-light cursor-pointer hover:border-primary border-transparent transform duration-150 border flex-col flex justify-between relative w-72 h-56 mr-6 px-8 py-6 rounded-3xl font-raleway"
+    >
       <div>
         <div className="flex">
           <TagCourse
@@ -16,7 +22,7 @@ const Card = ({ module }) => {
         <div className="text-xl ml-1 mt-5">{module.title}</div>
       </div>
 
-      <Progress total={module._exercices.length} current={0} />
+      <Progress total={module._exercices.length} current={nmbTests} />
     </div>
   );
 };
