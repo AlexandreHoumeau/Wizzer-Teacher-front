251473fb82f9bf891c0c8fc305f7d2ebcbcba75f
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-02-23 22:41:35 +0100

    updated navBar logo

--- src/routes/navbar.js
+++ src/routes/navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
-// import Logo from 'assets/images/logo_purple.png'
+
 const Navbar = () => {
   return (
     <div className="top-0 z-50 fixed bg-white flex flex-1 w-full p-5 shadow-lg">
       <a href="/">
-        {/* <img alt="logo" src={Logo} className="w-10" /> */}
+
       </a>
     </div>
   )
